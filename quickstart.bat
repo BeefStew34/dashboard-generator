74a9332d12635c2b2c74e1aac0c7324cf2db3be4
BeefStew <34359784+BeefStew34@users.noreply.github.com>
2026-07-09 16:01:21 +1200

    Building Plugin Steps established, plugin now shows on settings

--- quickstart.bat
+++ quickstart.bat
@@ -1,3 +1,9 @@
+rmdir /S /Q "%~dp0release\plugins-bundled\aut-dashboardgenerator-app"
+
+xcopy "%~dp0custom-plugins\aut-dashboardgenerator-app\dist\*" ^
+      "%~dp0release\plugins-bundled\aut-dashboardgenerator-app\" ^
+      /E /I /Y
+
 start /d "./pgsql/bin/" pg_ctl.exe -D ../data -U postgres start 
 start /d "./release/bin/" grafana-server.exe
 timeout /t 3
