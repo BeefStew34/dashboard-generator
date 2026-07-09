62845e1a63d16b6459fadd25b6550e53c2652d34
Nathan <34359784+BeefStew34@users.noreply.github.com>
2026-07-09 16:36:29 +1200

    Resolve Merge Conflict

--- quickstart.bat
+++ quickstart.bat
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 rmdir /S /Q "%~dp0release\plugins-bundled\aut-dashboardgenerator-app"
 
 xcopy "%~dp0custom-plugins\aut-dashboardgenerator-app\dist\*" ^
       "%~dp0release\plugins-bundled\aut-dashboardgenerator-app\" ^
       /E /I /Y
-
-=======
->>>>>>> 5d794cfe4a805972016238b1c6d5dc8f9cb13499
 start /d "./pgsql/bin/" pg_ctl.exe -D ../data -U postgres start 
 start /d "./release/bin/" grafana-server.exe
 timeout /t 3
