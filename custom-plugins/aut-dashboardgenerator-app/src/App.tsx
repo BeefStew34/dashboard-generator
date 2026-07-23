49a5e7e7f00a7b5312859aff5d405e9499695e9d
Nathan <34359784+BeefStew34@users.noreply.github.com>
2026-07-23 17:03:35 +1200

    Nathan (#4)
    * Add pages Generator and Sharing to the side bar. Updated Plugin.json
    
    * Generator.tsx skeleton
    
    * oops
    
    * dashboard preview skeleton

--- custom-plugins/aut-dashboardgenerator-app/src/App.tsx
+++ custom-plugins/aut-dashboardgenerator-app/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Button } from '@grafana/ui';
+import { AppRootProps } from '@grafana/data';
+import { GeneratorPage } from './Generator';
+import { SharingPage } from 'Sharing';
 
-export const App = () => {
+export const App = (props: AppRootProps) => {
+  const { path } = props;
+
+  if (path.endsWith('/creator')) return <GeneratorPage />;
+  if (path.endsWith('/sharing')) return <SharingPage />;
+
   return (
     <div>
-      <h1>AUT Dashboard Generator</h1>
-
-      <Button>
-        Test
-      </Button>
+      <h1>How to Use Instructions/Plugin Infomation/Plugin Configs</h1>
     </div>
   );
 };
