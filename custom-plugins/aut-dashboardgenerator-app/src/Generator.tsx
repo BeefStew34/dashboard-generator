cc1ed49b52d5cb00a7bfbdfbf8dfcb10c6d8d7f4
Nathan Kleine Deters <bqc6802@autuni.ac.nz>
2026-07-23 17:00:15 +1200

    dashboard preview skeleton

--- custom-plugins/aut-dashboardgenerator-app/src/Generator.tsx
+++ custom-plugins/aut-dashboardgenerator-app/src/Generator.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { Icon } from '@grafana/ui';
+import React, { useState } from 'react';
+import { Icon, Field, TextArea, Button } from '@grafana/ui';
 
 export const Heading = (txt: string) => {
-  return (<h2 style={{color: "#FFFFFF", backgroundColor:"#445c94", fontSize:"26px", textAlign:"center"}}>{txt}</h2>)
+  return (<h2 style={{color: "#FFFFFF", margin:"20px",backgroundColor:"#445c94", fontSize:"26px", justifyContent:"center", display: "flex", alignItems: "center", borderRadius:"15px 0 0 0", minHeight:"3em"}}>{txt}</h2>)
 }
+
+export const BigTextBox = () => {
+  const [value, setValue] = useState<string>('Test Test');
+
+  const TextAreaComponent = TextArea as any; // Work around for type problems
+
+  return (
+    <TextAreaComponent
+      value={value}
+      rows={6}
+      placeholder=""
+      onChange={(e: any) => setValue(e.currentTarget.value)}
+    />
+  );
+};
 
 export const GeneratorPage = () => {
   return (
@@ -14,9 +29,22 @@ export const GeneratorPage = () => {
     <Icon name="check" />;
     <h1>Create Dashboard</h1>
   </div>
-  <div style={{display: 'grid', padding:"10px", gridTemplateColumns:"1fr 1fr", gap:"50px", background:"0xFFFFFF"}}>
-    <div style={{gridColumn:"1", padding:"50px", background:"rgb(244, 245, 245)"}}>
+  <div style={{display: 'grid', padding:"50px", gridTemplateColumns:"1fr 1fr",gridTemplateRows:"1fr 1fr", gap:"50px"}}>
+    <div style={{gridColumn:"1", padding:"25px", background:"rgb(255, 255, 255)", borderRadius: "5px"}}>
       {Heading("Dashboard Preview")}
+      <div style={{background:"#F4F6F5", margin: "20px", padding: "10px", minHeight:"300px",minWidth:"300px", borderRadius: "5px"}}>
+
+      </div>
+      <div style={{background:"#F4F6F5", margin: "20px", padding: "10px"}}>
+        <h3>Describe your new dashboard!</h3>
+        <Field>
+          {BigTextBox()}
+        </Field>
+        <Button style={{ width: '100%', justifyContent: "center"}}>Generate</Button>
+      </div>
+      <div>
+        
+      </div>
     </div>
     
     <div style={{gridColumn:"2", flexGrow:"1", padding:"50px"}}>
